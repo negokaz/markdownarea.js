6669e0c0b72355c7549c57751648d3bdb9f7faeb
negokaz <negokaz@gmail.com>
2018-04-06 00:08:40 +0900

    feat: add toc

--- src/js/app.js
+++ src/js/app.js
@@ -95,12 +95,55 @@ function unindentText(text) {
 import 'github-markdown-css';
 import '../css/app.css';
 import 'highlight.js/styles/github.css';
+import '../css/toc.css';
+import './toc.js'
 
 // render markdown
 var view = jquery('<div class="markdown-body"></div>');
 var markdownSource = jquery('.markdownarea').text();
 view.html(markdown.render(unindentText(markdownSource)));
-jquery('body').append(view);
+// render toc
+var toc = jquery('<nav class="toc"></nav>');
+var index = 0;
+var currentLevel = 0;
+var currentElem = toc;
+var formatTocLi = function(headerElem) {
+    return jquery(`<li><a href="#${headerElem.get(0).id}">${headerElem.text()}</a></li>`);
+};
+var headers = view.find('h1,h2,h3').map(function() {
+    return {elem: jquery(this).clone().find('span.octicon').remove().end(), level: parseInt(this.localName.replace('h', ''))};
+}).each(function() {
+    var header = this;
+    if (header.level > currentLevel) {
+        var ul = jquery('<ul>');
+        if (toc.find('ul').length == 0) {
+            toc.append(ul.append(svg));
+        }
+        var li = formatTocLi(header.elem);
+        currentElem.append(ul.append(li));
+        currentElem = li;
+    } else if (header.level == currentLevel) {
+        var li = formatTocLi(header.elem);
+        currentElem.parent().append(li);
+        currentElem = li;
+    } else if (header.level < currentLevel) {
+        var li = formatTocLi(header.elem);
+        currentElem.parent().parent().append(li);
+        currentElem = li;
+    }
+    currentLevel = header.level;
+});
+var svg = 
+    jquery(`
+        <svg class="toc-marker" width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+            <path stroke="#444" stroke-width="3" fill="transparent" stroke-dasharray="0, 0, 0, 1000" stroke-linecap="round" stroke-linejoin="round" transform="translate(-0.5, -0.5)" />
+        </svg>
+    `);
+toc.append(svg);
+
+jquery('body')
+    .append(toc)
+    .append(view);
 
 // set title
 var title = view.find('h1');
